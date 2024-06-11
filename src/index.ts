9829f49efb3cc4928dbeff617fcb2124400d45f0
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-12 00:07:06 +0200

    runCLI

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+
 export { GHRelEasy, Config } from "./lib/release";
+
+const BUFFER = 10 * 1024 * 1024;
+
+export const runCli =
+  (name: string) =>
+  async (...ops: string[]) => {
+    const { stdout } = await promisify(exec)([name, ops].flat().join(" "), {
+      maxBuffer: BUFFER,
+      encoding: "utf-8",
+    });
+
+    return stdout.trim();
+  };

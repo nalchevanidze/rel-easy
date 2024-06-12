6ba915effa320c4d30bc44de93134b2af0e85414
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-12 15:17:58 +0200

    void

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -6,22 +6,21 @@ const BUFFER = 10 * 1024 * 1024;
 type Option = string | false | undefined;
 
 export class CLI {
+  constructor(private name: string) {}
 
-    constructor(private name:string){}
-  
-    void= async (...ops: Option[])=> {
-      console.log(await this.exec(...ops))
-    }
-  
-    exec = async (...ops: Option[]) => {
-      const { stdout } = await promisify(exec)(
-        [this.name, ops].filter(Boolean).flat().join(" "),
-        {
-          maxBuffer: BUFFER,
-          encoding: "utf-8",
-        }
-      );
-  
-      return stdout.trim();
-    };
-  }
+  void = async (...ops: Option[]) => {
+    console.log(await this.exec(...ops));
+  };
+
+  exec = async (...ops: Option[]) => {
+    const { stdout } = await promisify(exec)(
+      [this.name, ops].filter(Boolean).flat().join(" "),
+      {
+        maxBuffer: BUFFER,
+        encoding: "utf-8",
+      }
+    );
+
+    return stdout.trim();
+  };
+}
